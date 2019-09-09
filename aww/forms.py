7514708cef639c7d72b1ac32e6dc25ce023af085
willyowi <wilsonowino1@gmail.com>
2019-09-09 09:12:47 +0300

    sign up forms

--- aww/forms.py
+++ aww/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from aww.models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+# sign up forms
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
