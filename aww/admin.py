3da644dbfdefdebdf62f4a1f22947821f32e95d3
willyowi <wilsonowino1@gmail.com>
2019-09-09 09:14:24 +0300

    register profile

--- aww/admin.py
+++ aww/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from aww.models import *
 # Register your models here.
-
+# register profile
 admin.site.register(Profile)
 admin.site.register(Project)
 admin.site.register(Review)
