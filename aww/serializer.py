e6de04c591ca3bea38b4c2a1ca424a39fb9095a3
willyowi <wilsonowino1@gmail.com>
2019-09-09 09:01:36 +0300

    project serializer with fields and models

--- aww/serializer.py
+++ aww/serializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-
+# project serializer
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
