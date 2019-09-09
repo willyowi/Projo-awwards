0c85815566ea71815796820e6a550146258936e4
willyowi <wilsonowino1@gmail.com>
2019-09-09 09:13:19 +0300

    profile forms with model and exclude

--- aww/forms.py
+++ aww/forms.py
@@ -8,6 +8,7 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        # profile forms
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
