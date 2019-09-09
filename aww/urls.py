df2b2fd199547018eb772a0db73e3a4e1b139d4d
willyowi <wilsonowino1@gmail.com>
2019-09-09 08:53:48 +0300

    add urls for reg anfd logout

--- aww/urls.py
+++ aww/urls.py
@@ -19,8 +19,7 @@ urlpatterns = [
     url(r'api/project/project-id/(?P<pk>[0-9]+)/$',views.ProjectDescription.as_view()),
     url(r'^api/profile/$', views.ProfileList.as_view()),
     url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProfileDescription.as_view()),
-    # url(r'^api/project/$', views.ProjectList.as_view()),
-    # url(r'api/profile/profile-id/(?P<pk>[0-9]+)/$',views.ProjectDescription.as_view()),
+    
 
 
 
